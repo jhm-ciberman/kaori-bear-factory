ff26f8f8997bfccf07dde652ef9c1772a92e6215
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2020-03-02 09:42:46 -0300

    Fix bug with main menu camera animation

--- Assets/Scripts/UI/mainmenu/WindowUI.cs
+++ Assets/Scripts/UI/mainmenu/WindowUI.cs
@@ -4,6 +4,8 @@ public class WindowUI : MonoBehaviour
 {
     public event System.Action onClosed;
 
+    public RectTransform element = null;
+
     public void HideNow()
     {
         this.gameObject.SetActive(false);
@@ -12,6 +14,12 @@ public class WindowUI : MonoBehaviour
     public void Show()
     {
         this.gameObject.SetActive(true);
+
+        var rt = (this.element != null) ? this.element : this.GetComponent<RectTransform>();
+        var scale = rt.localScale;
+        rt.localScale = Vector3.zero;
+        LeanTween.scale(rt, scale, .5f)
+            .setEaseOutBack();
     }
 
     public void OnCloseButtonPressed()
